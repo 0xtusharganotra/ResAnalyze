f7acb3c31daa11a8f210bde66b6aec6b1e6a6b96
0xtusharganotra <ganotra.vox@gmail.com>
2025-08-13 20:48:26 +0530

    feat create homepage ui

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,37 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import Navbar from "../componenets/navbar";
+import { resumes } from "constants/index";
+import ResumeCard from "~/componenets/resumecard";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "ResAnalyze" },
+    {
+      name: "description",
+      content:
+        "AI-powered resume analyzer that scores your Resume and gives tailored recommendations based on the job description you provide.",
+    },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <main className="bg-[url('images/bg-main.svg')] bg-cover">
+      <Navbar />
+      <section className="main-section">
+        <div className="page-heading">
+          <h1>Track Your applications and resume ratintgs</h1>
+          <h2>Review Your subscription and check AI powered feedback</h2>
+        </div>
+      </section>
+
+      {resumes.length > 0 && (
+        <div className="resumes-section">
+          {resumes.map((resume, id) => (
+            <ResumeCard key={id} resume={resume} />
+          ))}
+        </div>
+      )}
+    </main>
+  );
 }
